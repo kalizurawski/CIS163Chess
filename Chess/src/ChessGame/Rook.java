5a4f74be4a61a31accc609970b163f5d9b89f449
Ben <benrand16@gmail.com>
2019-03-25 19:25:43 -0400

    Fixed Rook Jumping

--- Chess/src/ChessGame/Rook.java
+++ Chess/src/ChessGame/Rook.java
@@ -52,13 +52,13 @@ public class Rook extends ChessPiece {
                 valid = true;
             }
             if(move.toRow < move.fromRow){ // UP
-                if(move.toRow > move.fromRow){ // DOWN
-                    for(int n = 1; move.fromRow - n != move.toRow; n++){
-                        if(board[move.fromRow - n][move.fromColumn] != null){       //if up is not empty
-                            return valid;
-                        }
+                System.out.println("ayuh");
+                for(int n = 1; move.fromRow - n != move.toRow; n++){
+                    if(board[move.fromRow - n][move.fromColumn] != null){       //if up is not empty
+                        return valid;
                     }
                 }
+
                 valid = true;
             }
         }
